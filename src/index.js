aa2d3b8aa368221bd2466c6e8c0da43de4419bbb
Rodriguez Nadia <113744286+NaNajli@users.noreply.github.com>
2024-10-17 18:56:14 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 const p = document.createElement('p');
 const newmesa = document.querySelector('.root').appendChild(p);
-newmesa.innerText = '--Project with API Marvel--'
+newmesa.innerText = 'API Marvel'
 
+const baseURL = 'https://gateway.marvel.com/v1/public/comics?ts=1&apikey=b86e0dfc7ac1699752f05c1269c1234e&hash=0b50b7f09f080dcf97546c4e1f97b5b7'
 
+fetch(baseURL)
+  .then(response => response.json())
+  .then(data => console.log(data));
+  for (const character of json.data.results) {
+    let herourl = character.urls[0].url;
+  }
+  
