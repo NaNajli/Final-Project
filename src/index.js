3243c3a5721caeffee58ca3e715a66a0fe415edf
NaNajli <nadurodriguez2@gmail.com>
2024-10-19 10:33:59 -0300

    addingcharacter module

--- src/index.js
+++ src/index.js
@@ -1,31 +1,8 @@
+import { characters } from "./characters";
+import { getData } from "./utils";
 
 const p = document.createElement('p');
 const newmesa = document.querySelector('.root').appendChild(p);
 newmesa.innerText = 'API Marvel'
 
-//const baseURL = 'https://gateway.marvel.com/v1/public/comics?ts=1&apikey=b86e0dfc7ac1699752f05c1269c1234e&hash=0b50b7f09f080dcf97546c4e1f97b5b7'
-const url = 'https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=b86e0dfc7ac1699752f05c1269c1234e&hash=0b50b7f09f080dcf97546c4e1f97b5b7'
-
-const div = document.getElementById(".characters");
-const myList = document.querySelector(".list-characters");
-myList.append(div);
-const myRequest = new Request(url);
-
-fetch(myRequest)
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data)
-
-    for (const charact of  data.data.results) {
-  
-     const listItem = document.createElement("li");
-     listItem.appendChild(document.createElement("strong")).textContent = charact.name;
-     listItem.append(` ${charact.thumbnail.extension}.`);
-     listItem.appendChild(document.createElement("img")).src= `${charact.thumbnail.path}${charact.thumbnail.extension}`;
-     myList.appendChild(listItem);
-   
-  }
-  }
-)
-  .catch(console.error);
-
+characters();
