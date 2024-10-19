948ff6592cc04b5aabe89562ade085f888123a82
NaNajli <nadurodriguez2@gmail.com>
2024-10-19 15:39:04 -0300

    img-characters

--- src/characters.js
+++ src/characters.js
@@ -4,9 +4,11 @@ export function characters(data){
 //const baseURL = 'https://gateway.marvel.com/v1/public/comics?ts=1&apikey=b86e0dfc7ac1699752f05c1269c1234e&hash=0b50b7f09f080dcf97546c4e1f97b5b7'
 const url = 'https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=b86e0dfc7ac1699752f05c1269c1234e&hash=0b50b7f09f080dcf97546c4e1f97b5b7'
 
-const div = document.getElementById(".characters");
-const myList = document.querySelector(".list-characters");
+const div = document.getElementById('.characters');
+const myList = document.querySelector('.list-characters');
 myList.append(div);
+myList.appendChild(document.createElement('h1')).textContent = `Characters`;
+
 const myRequest = new Request(url);
 
 fetch(myRequest)
@@ -17,9 +19,9 @@ fetch(myRequest)
     for (const charact of  data.data.results) {
   
      const listItem = document.createElement("li");
-     listItem.appendChild(document.createElement("strong")).textContent = charact.name;
-     listItem.append(` ${charact.thumbnail.extension}.`);
-     listItem.appendChild(document.createElement("img")).src= `${charact.thumbnail.path}${charact.thumbnail.extension}`;
+     listItem.appendChild(document.createElement('h2')).textContent = charact.name;
+     listItem.append(``);
+     listItem.appendChild(document.createElement("img")).src= `${charact.thumbnail.path}.${charact.thumbnail.extension}`;
      myList.appendChild(listItem);
    
   }
