9d8f07c2011a41295ea57180c36f563448f1bbce
NaNajli <nadurodriguez2@gmail.com>
2024-10-22 10:32:23 -0300

    install lint

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { characters } from "./characters";
 
-const p = document.createElement('p');
-const newmesa = document.querySelector('.root').appendChild(p);
-newmesa.innerText = 'API Marvel'
+const p = document.createElement("p");
+const newmesa = document.querySelector(".root").appendChild(p);
+newmesa.innerText = "API Marvel";
 
 characters();
